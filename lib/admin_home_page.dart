e00bd701695a636e9da89f7ead1e3ce85787c2e0
mazin4070 <mazin4070@gmail.com>
2024-07-12 15:57:58 -0400

    new managment features

--- lib/admin_home_page.dart
+++ lib/admin_home_page.dart
@@ -1,16 +1,36 @@
-import 'package:admin/admin_login_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'admin_login_page.dart';
+import 'manage_vendor_service.dart';
+import 'manage_user_reservations.dart';
 
-class AdminHomePage extends StatelessWidget {
+class AdminHomePage extends StatefulWidget {
+  @override
+  _AdminHomePageState createState() => _AdminHomePageState();
+}
+
+class _AdminHomePageState extends State<AdminHomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    ManageVendorService(),
+    ManageUserReservations(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Admin Home'),
+        title: const Text('Admin Home'),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout),
+            icon: const Icon(Icons.logout),
             onPressed: () async {
               await FirebaseAuth.instance.signOut();
               Navigator.of(context).pushReplacement(
@@ -21,10 +41,22 @@ class AdminHomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text(
-          'Welcome, Admin!',
-          style: TextStyle(fontSize: 24),
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store),
+            label: 'Manage Vendors',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Manage Reservations',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
       ),
     );
   }
---
+++ lib/manage_user_reservations.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ManageUserReservations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Manage User Reservations',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}

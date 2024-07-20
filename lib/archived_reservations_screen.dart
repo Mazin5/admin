98152f7ba42234005bd5e49c867442ad025dcb44
mazin4070 <mazin4070@gmail.com>
2024-07-20 19:09:14 -0400

    some enhancments

--- lib/archived_reservations_screen.dart
+++ lib/archived_reservations_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ArchivedReservationsScreen extends StatefulWidget {
   @override
@@ -28,12 +29,40 @@ class _ArchivedReservationsScreenState extends State<ArchivedReservationsScreen>
         List<Map<String, dynamic>> tempArchivedReservations = [];
 
         for (var venueId in archivesMap.keys) {
-          Map<dynamic, dynamic> bookingsMap = Map<dynamic, dynamic>.from(archivesMap[venueId] as Map);
-          for (var key in bookingsMap.keys) {
-            Map<String, dynamic> bookingData = Map<String, dynamic>.from(bookingsMap[key] as Map);
-            bookingData['bookingId'] = key;
-            bookingData['venueId'] = venueId;
-            tempArchivedReservations.add(bookingData);
+          DatabaseReference venueArchivesRef = archivesRef.child(venueId);
+          DatabaseEvent venueArchivesEvent = await venueArchivesRef.once();
+          DataSnapshot venueArchivesSnapshot = venueArchivesEvent.snapshot;
+
+          if (venueArchivesSnapshot.value != null) {
+            Map<dynamic, dynamic> venueArchivesMap = Map<dynamic, dynamic>.from(venueArchivesSnapshot.value as Map);
+            for (var key in venueArchivesMap.keys) {
+              Map<String, dynamic> archiveData = Map<String, dynamic>.from(venueArchivesMap[key] as Map);
+              archiveData['bookingId'] = key;
+              archiveData['venueId'] = venueId;
+
+              // Fetch user data from Firestore using the userId
+              DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(archiveData['userId']).get();
+
+              if (userDoc.exists) {
+                archiveData['userFullName'] = '${userDoc['name']} ${userDoc['lastName']}';
+                archiveData['userName'] = userDoc['email'];
+                archiveData['phoneNumber'] = userDoc['phoneNumber'];
+              } else {
+                archiveData['userFullName'] = 'Not Available';
+                archiveData['userName'] = 'Not Available';
+                archiveData['phoneNumber'] = 'Not Available';
+              }
+
+              // Fetch venue data from Firestore
+              DocumentSnapshot venueDoc = await FirebaseFirestore.instance.collection('vendors').doc(venueId).get();
+              if (venueDoc.exists) {
+                archiveData['name'] = venueDoc['name'] ?? 'No Name';
+              } else {
+                archiveData['name'] = 'No Name';
+              }
+
+              tempArchivedReservations.add(archiveData);
+            }
           }
         }
 
@@ -43,7 +72,7 @@ class _ArchivedReservationsScreenState extends State<ArchivedReservationsScreen>
         });
         print('Archived reservations fetched: ${_archivedReservations.length}');
         _archivedReservations.forEach((reservation) {
-          print('Reservation: $reservation');
+          print('Archived reservation: $reservation');
         });
       } else {
         setState(() {
@@ -57,6 +86,25 @@ class _ArchivedReservationsScreenState extends State<ArchivedReservationsScreen>
       });
       print('Error fetching archived reservations: $error');
     }
+  }
+
+  void _showFullImage(BuildContext context, String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Dialog(
+            backgroundColor: Colors.transparent,
+            child: Container(
+              child: Image.network(imageUrl),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -80,13 +128,15 @@ class _ArchivedReservationsScreenState extends State<ArchivedReservationsScreen>
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('Venue ID: ${reservation['venueId']}', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                            Text('Venue: ${reservation['name']}', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                             SizedBox(height: 5),
                             Text('Date: ${reservation['date']}', style: TextStyle(fontSize: 16)),
                             SizedBox(height: 5),
-                            Text('User ID: ${reservation['userId']}', style: TextStyle(fontSize: 16)),
+                            Text('User Email: ${reservation['userName']}', style: TextStyle(fontSize: 16)),
                             SizedBox(height: 5),
                             Text('User Full Name: ${reservation['userFullName']}', style: TextStyle(fontSize: 16)),
+                            SizedBox(height: 5),
+                            Text('User Phone Number: ${reservation['phoneNumber']}', style: TextStyle(fontSize: 16)),
                             SizedBox(height: 5),
                             Text('Transaction Code: ${reservation['transactionCode']}', style: TextStyle(fontSize: 16)),
                             SizedBox(height: 5),
@@ -95,7 +145,14 @@ class _ArchivedReservationsScreenState extends State<ArchivedReservationsScreen>
                             Text('Status: ${reservation['status']}', style: TextStyle(fontSize: 16)),
                             SizedBox(height: 10),
                             reservation['receiptImageUrl'] != null
-                                ? Image.network(reservation['receiptImageUrl'])
+                                ? GestureDetector(
+                                    onTap: () => _showFullImage(context, reservation['receiptImageUrl']),
+                                    child: Image.network(
+                                      reservation['receiptImageUrl'],
+                                      width: 100, // Set smaller width
+                                      height: 100, // Set smaller height
+                                    ),
+                                  )
                                 : Text('No receipt image available'),
                           ],
                         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:admin/provider/vendor_provider.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'admin_login_page.dart';
 
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Initialize Firebase App Check
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
   );
   runApp(
     MultiProvider(
@@ -28,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AdminLoginPage(), // Set the admin login page as the home screen
+      home: AdminLoginPage(), 
     );
   }
 }

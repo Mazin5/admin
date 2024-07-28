1028839a61fbcd3d273f95b832cc782b75118fcf
Yahya Odhari <yahyaalothri@gmail.com>
2024-07-28 19:57:47 +0300

    hoh

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
-import 'package:admin/provider/vendor_provider.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'admin_login_page.dart';
-
-
+import 'provider/vendor_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -21,7 +24,64 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _initializeFCM();
+  }
+
+  void _initializeFCM() async {
+    FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+    // Request permissions for iOS
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+      _getToken();
+    } else {
+      print('User declined or has not accepted permission');
+    }
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received a message in the foreground!');
+      print('Message data: ${message.data}');
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Message clicked!');
+    });
+  }
+
+  void _getToken() async {
+    FirebaseMessaging _messaging = FirebaseMessaging.instance;
+    String? token = await _messaging.getToken();
+    print('FCM Token: $token');
+
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+   String? adminId= _auth.currentUser?.uid;
+
+
+    // Save the token to Firestore
+    if (token != null) {
+      await FirebaseFirestore.instance.collection('admins').doc(adminId).set({'token': token},SetOptions(merge: true));
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,7 +89,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AdminLoginPage(), 
+      home: AdminLoginPage(),
     );
   }
 }
